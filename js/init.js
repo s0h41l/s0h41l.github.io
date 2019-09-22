99dd60fae1651bce1a40a30d8230234c58466c43
Sohail <sohailkhan.sk@hotmail.com>
2019-09-23 00:22:50 +0500

    carousal set

--- js/init.js
+++ js/init.js
@@ -358,20 +358,20 @@ function arlo_tm_owl_carousel(){
 			lazyLoad: true,
 			margin: 30,
 			autoplay: false,
-			autoplayTimeout: 3000,
-			smartSpeed: 1000,
+			autoplayTimeout: 6000,
+			smartSpeed: 2000,
 			dots: true,
 			nav: false,
 			navSpeed: true,
-			responsive:{
-			0:{items:1},
-			480:{items:2},
-			768:{items:3},
-			1040:{items:3},
-			1200:{items:3},
-			1600:{items:3},
-			1920:{items:3}
-		}
+		// 	responsive:{
+		// 	0:{items:1},
+		// 	480:{items:2},
+		// 	768:{items:3},
+		// 	1040:{items:3},
+		// 	1200:{items:3},
+		// 	1600:{items:3},
+		// 	1920:{items:3}
+		// }
 	});
 	
 	jQuery('.arlo_tm_services_wrap .custom_nav > a.prev').on('click', function(){
